e8a156f9a4f8ea6362fb338addea4a13fc9f74a8
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-05-02 13:33:56 -0300

    feat: basic landing page with features

--- docs/src/app/(home)/layout.tsx
+++ docs/src/app/(home)/layout.tsx
@@ -2,10 +2,8 @@ import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <main className="flex flex-col max-w-7xl w-full border mx-auto h-[200vh]">
-        {children}
-      </main>
-    </>
+    <div className="flex flex-col w-screen overflow-x-hidden">
+      <div className="flex flex-col max-w-7xl w-full mx-auto">{children}</div>
+    </div>
   );
 }
