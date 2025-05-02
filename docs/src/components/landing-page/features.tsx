c0c4153791fe26457f197c6bc641f854412b7bf9
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-05-02 13:33:56 -0300

    feat: basic landing page with features

---
+++ docs/src/components/landing-page/features.tsx
@@ -0,0 +1,98 @@
+import { cn } from "@/lib/utils";
+import { CloudOff, DatabaseZap, Flame, Package } from "lucide-react";
+
+export function Features() {
+  const features = [
+    {
+      title: "Offline-first",
+      description: "Save changes locally and sync when you're back online.",
+      icon: <CloudOff />,
+    },
+    {
+      title: "Fine-grained reactivity",
+      description: "React only when the specific data you need changes.",
+      icon: <Flame />,
+    },
+    {
+      title: "Built-in ORM",
+      description: "Built-in ORM to design and interact with your database.",
+      icon: <Package />,
+    },
+    {
+      title: "Works with any database",
+      description:
+        "Sync works with any database, be it a NoSQL or a SQL database.",
+      icon: <DatabaseZap />,
+    },
+    {
+      title: "Offline-first 2",
+      description: "Save changes locally and sync when you're back online.",
+      icon: <CloudOff />,
+    },
+    {
+      title: "Fine-grained reactivity 2",
+      description: "React only when the specific data you need changes.",
+      icon: <Flame />,
+    },
+    {
+      title: "Built-in ORM 2",
+      description: "Built-in ORM to design and interact with your database.",
+      icon: <Package />,
+    },
+    {
+      title: "Works with any database 2",
+      description:
+        "Sync works with any database, be it a NoSQL or a SQL database.",
+      icon: <DatabaseZap />,
+    },
+  ];
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 text-left relative z-10 max-w-7xl mx-auto border-t">
+      {features.map((feature, index) => (
+        <Feature key={feature.title} {...feature} index={index} />
+      ))}
+    </div>
+  );
+}
+
+const Feature = ({
+  title,
+  description,
+  icon,
+  index,
+}: {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+  index: number;
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col md:border-r md:border-b py-10 relative group/feature",
+        index % 2 === 0 && "md:border-l lg:border-l-0",
+        index % 4 === 0 && "lg:border-l",
+        index < 4 && "lg:border-b"
+      )}
+    >
+      {index < 4 && (
+        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+      )}
+      {index >= 4 && (
+        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+      )}
+      <div className="mb-4 relative z-10 px-10 text-neutral-600 dark:text-neutral-400">
+        {icon}
+      </div>
+      <div className="text-lg font-bold mb-2 relative z-10 px-10">
+        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
+        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-800 dark:text-neutral-100">
+          {title}
+        </span>
+      </div>
+      <p className="text-sm text-neutral-600 dark:text-neutral-300 max-w-xs relative z-10 px-10">
+        {description}
+      </p>
+    </div>
+  );
+};
