730c40bd1935463b596fb4f5d38ce82c204083e7
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-05-29 23:01:16 -0300

    chore: cleanup

--- packages/live-state/src/server/transport-layers/http.ts
+++ packages/live-state/src/server/transport-layers/http.ts
@@ -106,8 +106,6 @@ export const httpTransportLayer = (
           body = data;
         }
 
-        console.log("[HTTP] mutation result", rawBody, body);
-
         const result = await server.handleRequest({
           req: {
             ...requestContext,
@@ -135,106 +133,5 @@ export const httpTransportLayer = (
       { message: "Not found", code: "NOT_FOUND" },
       { status: 404 }
     );
-
-    // const reply = (msg: ServerMessage) => {
-    //   ws.send(JSON.stringify(msg));
-    // };
-    // // TODO add ability to refuse connection
-    // const requestContext: {
-    //   headers: Record<string, any>;
-    //   cookies: Record<string, any>;
-    // } = {
-    //   headers: request.headers,
-    //   cookies:
-    //     typeof request.headers.cookie === "string"
-    //       ? cookie.parse(request.headers.cookie)
-    //       : {},
-    // };
-    // connections[clientId] = ws;
-    // console.log("Client connected:", clientId);
-    // ws.on("message", async (message) => {
-    //   try {
-    //     console.log("Message received from the client:", message);
-    //     const parsedMessage = clientMessageSchema.parse(
-    //       JSON.parse(message.toString())
-    //     );
-    //     if (parsedMessage.type === "SUBSCRIBE") {
-    //       const { resource } = parsedMessage;
-    //       if (!subscriptions[resource]) subscriptions[resource] = {};
-    //       subscriptions[resource][clientId] = {};
-    //       // TODO send bootstrap
-    //     } else if (parsedMessage.type === "SYNC") {
-    //       const { resources: _res } = parsedMessage;
-    //       const resources = _res ?? Object.keys(server.schema);
-    //       console.log("Syncing resources:", resources);
-    //       await Promise.all(
-    //         resources.map(async (resourceName) => {
-    //           const result = await server.handleRequest({
-    //             req: {
-    //               ...requestContext,
-    //               type: "QUERY",
-    //               resourceName,
-    //               context: {}, // TODO provide context
-    //             },
-    //           });
-    //           if (!result || !result.data) {
-    //             throw new Error("Invalid resource");
-    //           }
-    //           reply({
-    //             id: parsedMessage.id,
-    //             type: "SYNC",
-    //             resource: resourceName,
-    //             data: Object.fromEntries(
-    //               Object.entries(
-    //                 (result.data ?? {}) as Record<
-    //                   string,
-    //                   MaterializedLiveType<LiveObjectAny>
-    //                 >
-    //               ).map(([id, v]) => [id, v.value])
-    //             ),
-    //           });
-    //         })
-    //       );
-    //     } else if (parsedMessage.type === "MUTATE") {
-    //       const { resource } = parsedMessage;
-    //       console.log("Received mutation from client:", parsedMessage);
-    //       try {
-    //         const result = await server.handleRequest({
-    //           req: {
-    //             ...requestContext,
-    //             type: "MUTATE",
-    //             resourceName: resource,
-    //             input: parsedMessage.payload,
-    //             context: { messageId: parsedMessage.id }, // TODO provide context
-    //             resourceId: (parsedMessage as DefaultMutation).resourceId,
-    //             procedure: (parsedMessage as GenericMutation).procedure,
-    //           },
-    //         });
-    //         if ((parsedMessage as GenericMutation).procedure) {
-    //           reply({
-    //             id: parsedMessage.id,
-    //             type: "REPLY",
-    //             data: result,
-    //           });
-    //         }
-    //       } catch (e) {
-    //         reply({
-    //           id: parsedMessage.id,
-    //           type: "REJECT",
-    //           resource,
-    //           message: (e as Error).message,
-    //         });
-    //         console.error("Error parsing mutation from the client:", e);
-    //       }
-    //     }
-    //   } catch (e) {
-    //     // TODO send error to client
-    //     console.error("Error handling message from the client:", e);
-    //   }
-    // });
-    // ws.on("close", () => {
-    //   console.log("Connection closed", clientId);
-    //   delete connections[clientId];
-    // });
   };
 };
