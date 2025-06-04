65169a6ade34649202207673b6af5390678433c1
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-06-04 18:29:29 -0300

    chore: add generatable util

--- packages/live-state/src/core/utils.ts
+++ packages/live-state/src/core/utils.ts
@@ -3,3 +3,16 @@ import { ulid } from "ulid";
 export const generateId = () => ulid().toLowerCase();
 
 export type Promisify<T> = T extends Promise<any> ? T : Promise<T>;
+
+export type Awaitable<T> = T | Promise<T>;
+
+export type Generatable<T, Arg = never> = T | ((arg: Arg) => T);
+
+export const consumeGeneratable = <T, Arg = never>(
+  value: Generatable<T, Arg>,
+  arg?: Arg
+): T => {
+  return typeof value === "function"
+    ? (value as (arg: Arg) => T)(arg as Arg)
+    : value;
+};
