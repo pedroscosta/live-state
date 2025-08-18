98aba9fa8878f4ce8c350f126a651141958f9b2f
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-08-18 18:14:20 -0300

    chore: remove InMemoryStorage

--- packages/live-state/src/server/storage.ts
+++ packages/live-state/src/server/storage.ts
@@ -31,53 +31,6 @@ export abstract class Storage {
   ): Promise<MaterializedLiveType<T>>;
 }
 
-export class InMemoryStorage extends Storage {
-  private storage: Record<string, Record<string, any>> = {};
-
-  public async updateSchema(opts: Schema<any>): Promise<void> {
-    this.storage = Object.entries(opts).reduce(
-      (acc, [_, entity]) => {
-        acc[entity.name] = {};
-        return acc;
-      },
-      {} as typeof this.storage
-    );
-  }
-
-  public async findById<T extends LiveObjectAny>(
-    resourceName: string,
-    id: string
-  ): Promise<MaterializedLiveType<T> | undefined> {
-    return this.storage[resourceName]?.[id] as MaterializedLiveType<T>;
-  }
-
-  public async find<T extends LiveObjectAny>(
-    resourceName: string,
-    where?: Record<string, any>,
-    include?: Record<string, any>
-  ): Promise<Record<string, MaterializedLiveType<T>>> {
-    // TODO implement where conditions
-    // TODO implement include conditions
-
-    return (this.storage[resourceName] ?? {}) as Record<
-      string,
-      MaterializedLiveType<T>
-    >;
-  }
-
-  public async upsert<T extends LiveObjectAny>(
-    resourceName: string,
-    resourceId: string,
-    value: MaterializedLiveType<T>
-  ): Promise<MaterializedLiveType<T>> {
-    this.storage[resourceName] ??= {};
-
-    this.storage[resourceName][resourceId] = value;
-
-    return value;
-  }
-}
-
 type SimpleKyselyQueryInterface = {
   where: (
     column: string,
