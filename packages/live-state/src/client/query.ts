a09bdf062e822f16d78742ffa4d97fa68174567d
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-09-04 00:16:33 -0300

    feat(client): reflect one and first on the return type of the query

--- packages/live-state/src/client/query.ts
+++ packages/live-state/src/client/query.ts
@@ -1,3 +1,4 @@
+/** biome-ignore-all lint/complexity/noBannedTypes: <explanation> */
 import type { RawQueryRequest } from "../core/schemas/core-protocol";
 import type {
   IncludeClause,
@@ -15,28 +16,40 @@ export type QueryExecutor = {
   ): () => void;
 };
 
+type InferQueryResult<
+  TCollection extends LiveObjectAny,
+  TInclude extends IncludeClause<TCollection>,
+  TSingle extends boolean = false,
+> = TSingle extends true
+  ? Simplify<InferLiveObject<TCollection, TInclude>> | undefined
+  : Simplify<InferLiveObject<TCollection, TInclude>>[];
+
 export class QueryBuilder<
   TCollection extends LiveObjectAny,
   TInclude extends IncludeClause<TCollection> = {},
+  TSingle extends boolean = false,
 > {
   private _collection: TCollection;
   private _client: QueryExecutor;
   private _where: WhereClause<TCollection>;
   private _include: TInclude;
   private _limit?: number;
+  private _single?: TSingle;
 
   private constructor(
     collection: TCollection,
     client: QueryExecutor,
     where?: WhereClause<TCollection>,
     include?: TInclude,
-    limit?: number
+    limit?: number,
+    single?: TSingle
   ) {
     this._collection = collection;
     this._client = client;
     this._where = where ?? {};
     this._include = include ?? ({} as TInclude);
     this._limit = limit;
+    this._single = single;
 
     this.get = this.get.bind(this);
     this.subscribe = this.subscribe.bind(this);
@@ -47,14 +60,16 @@ export class QueryBuilder<
       this._collection,
       this._client,
       { ...this._where, ...where },
-      this._include
+      this._include,
+      this._limit,
+      this._single
     );
   }
 
   include<TNewInclude extends IncludeClause<TCollection>>(
     include: TNewInclude
   ) {
-    return new QueryBuilder<TCollection, TInclude & TNewInclude>(
+    return new QueryBuilder<TCollection, TInclude & TNewInclude, TSingle>(
       this._collection,
       this._client,
       this._where,
@@ -62,24 +77,26 @@ export class QueryBuilder<
         ...this._include,
         ...include,
       },
-      this._limit
+      this._limit,
+      this._single
     );
   }
 
-  get(): Simplify<InferLiveObject<TCollection, TInclude>>[] {
+  get(): InferQueryResult<TCollection, TInclude, TSingle> {
     const result = this._client.get({
       resource: this._collection.name,
       where: this._where,
       include: this._include,
       limit: this._limit,
     });
-    return result;
+
+    if (this._single) return result[0];
+
+    return result as InferQueryResult<TCollection, TInclude, TSingle>;
   }
 
   subscribe(
-    callback: (
-      value: Simplify<InferLiveObject<TCollection, TInclude>>[]
-    ) => void
+    callback: (value: InferQueryResult<TCollection, TInclude, TSingle>) => void
   ): () => void {
     return this._client.subscribe(
       {
@@ -88,7 +105,11 @@ export class QueryBuilder<
         include: this._include,
         limit: this._limit,
       },
-      callback
+      (v) => {
+        if (this._single) return callback(v[0]);
+
+        callback(v as InferQueryResult<TCollection, TInclude, TSingle>);
+      }
     );
   }
 
@@ -98,16 +119,24 @@ export class QueryBuilder<
       this._client,
       this._where,
       this._include,
-      limit
+      limit,
+      this._single
     );
   }
 
   one(id: string) {
-    return this.where({ id }).limit(1);
+    return this.first({ id });
   }
 
   first(where?: WhereClause<TCollection>) {
-    return where ? this.where(where).limit(1) : this.limit(1);
+    return new QueryBuilder(
+      this._collection,
+      this._client,
+      where,
+      this._include,
+      1,
+      true
+    );
   }
 
   toJSON() {
