280854b200b22d4b8c602a70a1067a692180b55e
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-10-10 19:25:56 -0300

    docs: write section about using live-state (#73)

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -1,12 +1,23 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { rehypeCodeDefaultOptions } from "fumadocs-core/mdx-plugins";
+import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import { transformerTwoslash } from "fumadocs-twoslash";
+import { createFileSystemTypesCache } from 'fumadocs-twoslash/cache-fs';
 
 // Options: https://fumadocs.vercel.app/docs/mdx/collections#define-docs
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
 });
 
 export default defineConfig({
   mdxOptions: {
-    // MDX options
+    rehypeCodeOptions: {
+      ...rehypeCodeDefaultOptions,
+      transformers: [
+        ...(rehypeCodeDefaultOptions.transformers ?? []),
+        transformerTwoslash({
+          typesCache: createFileSystemTypesCache(),
+        }),
+      ],
+    },
   },
 });
